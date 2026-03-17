78f58666e0b05495c1768d6cbffb3cb0315aa14c
pannawichl-bit <pannawich.l@ku.th>
2026-03-17 20:11:36 +0700

    Add

---
+++ Fronend/js/register.js
@@ -0,0 +1,24 @@
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+
+    e.preventDefault()
+
+    const username = document.getElementById("username").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const confirmPassword = document.getElementById("confirmPassword").value
+
+    if (password !== confirmPassword) {
+        alert("รหัสผ่านไม่ตรงกัน")
+        return
+    }
+
+    alert("สมัครสมาชิกสำเร็จ")
+
+})
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+
+    e.preventDefault()
+
+    alert("สมัครสมาชิกสำเร็จ")
+
+})
